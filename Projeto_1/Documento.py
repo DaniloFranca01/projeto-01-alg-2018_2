3c043c97b6949c51ecc2b5ed426637d4eede953e
danilo <dlf3@cin.ufpe.br>
2018-10-01 01:00:09 -0300

    Continuação da implementaçaõ da Classe Documento

--- Projeto_1/Documento.py
+++ Projeto_1/Documento.py
@@ -1,27 +1,34 @@
+import numpy as np
 class Documento():
     
     def __init__(self,diretorio):
-        self.lista= []
         self.diretorio=diretorio
+        self.nPalavras = 0
+        self.vPalavras = None
     
     def __str__(self):
-        return str(self.lista)
+        return str(self.vPalavras)
     
     def __repr__(self):
         return self.__str__()
     
     def carregarDoc(self):
-        specChars = ("\n",":",".","<",">","(",")","/",'"',",")
+        lista = []
         arq=open(self.diretorio,'r')
         arq=arq.read()
         palavra = ""
         for i in arq:
-            if (not i in specChars) and (i != " "):
+            cod = ord(i)
+            if (cod >= 65 and cod<=90) or (cod >= 97 and cod<=122):
                 palavra += i
-            elif i == " ":
-                self.lista.append(palavra)
-                palavra = "" 
-                
+            elif i == " " and palavra !="":
+                lista.append(palavra)
+                self.nPalavras +=1
+                palavra = ""
+        
+        self.vPalavras = np.array(lista, dtype=object)
+        
+        
               
 a = Documento("C:\\Users\danilo.DESKTOP-8QL5HFM\Downloads\Projeto 1 ALG\dados\src\source-document00010.txt")
 a.carregarDoc()
